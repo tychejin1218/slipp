59b83bf7edd433ab6ddac37b9d7fa2e48174f760
tychejin1218 <tychejin@naver.com>
2020-03-30 16:58:08 +0900

    [SLiPP] logback 설정

--- qna/src/main/java/net/slipp/web/HomeController.java
+++ qna/src/main/java/net/slipp/web/HomeController.java
@@ -1,14 +1,19 @@
 package net.slipp.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
 	@RequestMapping("/")
 	public String home() {
-		// prefix("/") + return(home) + suffix(".jsp")
+		log.debug("logback setting start!");
+		//prefix("/") + return(home) + suffix(".jsp")
 		return "home";
 	}
 }
