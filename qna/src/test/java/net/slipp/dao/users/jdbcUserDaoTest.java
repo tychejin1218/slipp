c910aa6f7cd30186074ead641585905ec65fd439
tychejin1218 <tychejin@naver.com>
2020-04-15 10:59:09 +0900

    [SLiPP] DAO Test에서 Transaction 기능 활용

--- qna/src/test/java/net/slipp/dao/users/jdbcUserDaoTest.java
+++ qna/src/test/java/net/slipp/dao/users/jdbcUserDaoTest.java
@@ -10,11 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import net.slipp.domain.users.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/applicationContext.xml")
+@TransactionConfiguration(transactionManager="transactionManager", defaultRollback=true)
 public class jdbcUserDaoTest {
 
 	private static final Logger log = LoggerFactory.getLogger(jdbcUserDaoTest.class);
@@ -24,16 +27,16 @@ public class jdbcUserDaoTest {
 
 	@Test
 	public void findById() {
-		User user = userDao.findById("tychejin");
+		User user = userDao.findById("admin");
 		log.debug("User : {}", user);
 	}
 
 	@Test
+	@Transactional
 	public void create() {
-		User user = new User("tychejin2", "password", "박진영1", "tychejin2@naver.com");
+		User user = new User("admin1", "password", "관리자", "admin1@naver.com");
 		userDao.create(user);
-		User actual = userDao.findById(user.getUserId());
-		
+		User actual = userDao.findById(user.getUserId());		
 		assertThat(actual, is(user));
 	}
 }
